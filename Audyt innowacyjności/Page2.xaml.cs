cc13cf51ecf8ac007205a889f439974a89a3bfd0
Pszem <Pszem@DESKTOP-7QEDT7R>
2018-08-12 11:40:01 +0200

    update validation rules

--- Audyt innowacyjności/Page2.xaml.cs
+++ Audyt innowacyjności/Page2.xaml.cs
@@ -31,11 +31,9 @@ namespace Audyt_innowacyjności
         private void Next(object sender, RoutedEventArgs e)
         {
 
-            bool valid = model.IsValid ?? false;
-            if (valid)
+
                 this.NavigationService.ONavigate(new Page3(), model);
-            else
-                validator.Content = model.ValidationErrorsString;
+
         }
 
         private void Back(object sender, RoutedEventArgs e)
