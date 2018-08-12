515f3011a21e45a7e2876a76dded58e15aa6f552
Pszem <Pszem@DESKTOP-7QEDT7R>
2018-08-12 17:44:54 +0200

    Add validation string to Xaml

---
+++ Audyt innowacyjności/Migrations/201808121538470_mg.cs
@@ -0,0 +1,18 @@
+namespace Audyt_innowacyjności.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mg : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Surveys", "NumerKRS", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Surveys", "NumerKRS", c => c.Int(nullable: false));
+        }
+    }
+}
