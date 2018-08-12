1ab82cac5ea42533dde49a64df54565fc193fb34
Pszem <Pszem@DESKTOP-7QEDT7R>
2018-08-12 13:28:31 +0200

    Update Chart view

---
+++ Audyt innowacyjności/Extensions/IntExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audyt_innowacyjności.Extensions
+{
+   public static class IntExtensions
+    {
+
+
+
+        public static int Map(this int value,
+            int originalStart, int originalEnd, // original range
+            int newStart, int newEnd // desired range
+            ) // value to convert
+        {
+            if (value > originalEnd)
+                return newEnd;
+            if (value < originalStart)
+            {
+                return newStart;
+
+            }
+            double scale = (double)(newEnd - newStart) / (originalEnd - originalStart);
+            return (int)(newStart + ((value - originalStart) * scale));
+
+        }
+
+
+    }
+
+}
